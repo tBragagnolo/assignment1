defd517a3fb0546c48ce0876547f3b7d79f1f3e0
tbragagnolo <tbragagnolo@myseneca.ca>
2023-01-21 09:41:39 -0500

    update

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 
+const app = express();
 const port = 8080;
 
 function onStart() {
     console.log("Listening on:", port);
 }
+
+app.use(cors());
 
 app.get("/", (req, res)=>{
     res.json({message:"API Listening"});
