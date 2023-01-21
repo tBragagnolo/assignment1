1c42e12f3261d0f167bac9f7cd18688fe2e91833
tbragagnolo <tbragagnolo@myseneca.ca>
2023-01-21 09:51:02 -0500

    update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv").config;
+const MoviesDB = require("./modules/moviesDB");
 
 const app = express();
 const port = 8080;
